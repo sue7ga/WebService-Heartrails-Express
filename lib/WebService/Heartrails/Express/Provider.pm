bad98cc50d8896252d9ea49a025e2ec94800f21e
sue7ga <sue77ga@gmail.com>
2014-08-28 05:10:47 -0700

    new

---
+++ lib/WebService/Heartrails/Express/Provider.pm
@@ -0,0 +1,22 @@
+package WebService::Heartrails::Express::Provider;
+use strict;
+use warnings;
+use utf8;
+use Mouse;
+use Furl;
+use WebService::Heartrails::Express::Provider::Line;
+use WebService::Heartrails::Express::Provider::Station;
+use WebService::Heartrails::Express::Provider::Near;
+
+has furl => (
+  is => 'ro',
+ isa => 'Furl', 
+);
+
+sub dispatch{
+  my($self,$api_name,$arg) = @_;
+  my $class = __PACKAGE__.'::'.ucfirst($api_name);
+  $class->call($self,$arg);
+}
+
+1;
